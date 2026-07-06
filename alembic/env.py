e9fcca2595d397c1bdd0d618045241fbb4f5dae6
amaldas-interloid <amaldas@interloid.com>
2026-07-06 19:44:55 +0530

    feat: add User model and initial migration

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 from app.core.config import settings
 from app.db.base import Base
-
+from app import models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
